7987b91173a079d7e2cfe6cdb76ca655233df473
Steff Gornall <steffgornall@gmail.com>
2024-07-30 13:28:56 +0100

    Syled hero text and footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-black text-white w-screen flex p-10 bg-opacity-80">
-      <h1>I am link</h1>Footer
+    <div className="bg-black text-white w-screen items-center justify-center flex p-10">
+      <div className="flex w-2/3">
+        <div className="flex flex-col gap-4">
+          <h1>I am link</h1>
+          <h1>I am link</h1>
+          <h1>I am link</h1>
+        </div>
+      </div>
     </div>
   );
 };
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,9 +4,13 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="flex flex-col justify-center items-center h-4/5 gap-14">
-      <h1 className="font-extrabold text-8xl">Expand your vocabulary.</h1>
+      <h1 className="font-extrabold text-8xl">
+        <span className="heroLetters">Expand</span> {""}
+        <span className="heroLetters">your</span> {""}
+        <span className="heroLetters">Vocabulary.</span>
+      </h1>
       <SignInButton>
-        <button className="actionButton p-4 font-semibold">
+        <button className="actionButton p-4 font-semibold uppercase">
           Sign in to get started
         </button>
       </SignInButton>
